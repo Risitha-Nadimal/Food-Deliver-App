2ef5b092a6b38345d6703b941d2b906da8190ca8
Risitha Nadimal <102137397+Risitha-Nadimal@users.noreply.github.com>
2024-02-01 19:33:10 +0530

    edit

---
+++ food_deliver_app/lib/Scrren/splashscrren/splashscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashScrren extends StatefulWidget {
+  const SplashScrren({super.key});
+
+  @override
+  State<SplashScrren> createState() => _SplashScrrenState();
+}
+
+class _SplashScrrenState extends State<SplashScrren> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container());
+  }
+}
