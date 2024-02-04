7ca2182ce2790bbc00e1853d9fff1a741c37f48f
Risitha Nadimal <102137397+Risitha-Nadimal@users.noreply.github.com>
2024-02-04 18:09:52 +0530

    splash screen added

---
+++ food_deliver_app/lib/Scrren/splashscrren/getting_start_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GettingStart extends StatelessWidget {
+  const GettingStart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container());
+  }
+}
--- food_deliver_app/lib/Scrren/splashscrren/splashscreen.dart
+++ food_deliver_app/lib/Scrren/splashscrren/splashscreen.dart
@@ -1,4 +1,6 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:food_deliver_app/Scrren/splashscrren/getting_start_page.dart';
 
 class SplashScrren extends StatefulWidget {
   const SplashScrren({super.key});
@@ -9,7 +11,39 @@ class SplashScrren extends StatefulWidget {
 
 class _SplashScrrenState extends State<SplashScrren> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const GettingStart(),
+          ));
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: SizedBox(
+      width: size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Roulette(
+              child: Image.asset("assests/images/food-safety.png", scale: 4)),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            "No wating for Food",
+            style: TextStyle(fontSize: 14, color: Color(0xff838383)),
+          )
+        ],
+      ),
+    ));
   }
 }
