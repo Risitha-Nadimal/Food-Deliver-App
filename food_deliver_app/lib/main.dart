f169f62d51f57676316967455929af5e729c99d8
Risitha Nadimal <102137397+Risitha-Nadimal@users.noreply.github.com>
2024-02-04 16:13:51 +0530

    Update main.dart

--- food_deliver_app/lib/main.dart
+++ food_deliver_app/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Food Deliver App',
       theme: ThemeData(),
       debugShowCheckedModeBanner: false,
       home: const SplashScrren(),
