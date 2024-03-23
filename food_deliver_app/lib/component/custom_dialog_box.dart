368b1a8bdc1c9860ce22f09e573c61b5e4abfb1d
Risitha Nadimal <102137397+Risitha-Nadimal@users.noreply.github.com>
2024-03-23 14:41:52 +0530

    awesome dialog

---
+++ food_deliver_app/lib/component/custom_dialog_box.dart
@@ -0,0 +1,21 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:flutter/material.dart';
+
+class DialogBox {
+  Future<dynamic> dialogbox(
+    BuildContext context,
+    DialogType dialogType,
+    String title,
+    String desc,
+  ) async {
+    return AwesomeDialog(
+      context: context,
+      dialogType: dialogType,
+      animType: AnimType.rightSlide,
+      title: title,
+      desc: desc,
+      btnCancelOnPress: () {},
+      btnOkOnPress: () {},
+    ).show();
+  }
+}
