19572f5525ae12fc5a7385519217cb5c5b94b0df
Risitha Nadimal <102137397+Risitha-Nadimal@users.noreply.github.com>
2024-06-13 10:47:15 +0530

    1

--- food_deliver_app/lib/Scrren/main_screen/resturentdetails_screen/resturent_details_screen.dart
+++ food_deliver_app/lib/Scrren/main_screen/resturentdetails_screen/resturent_details_screen.dart
@@ -11,11 +11,22 @@ class RestaruntDetailsScreen extends StatefulWidget {
 class _RestaruntDetailsScreenState extends State<RestaruntDetailsScreen> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
         body: Container(
-      child: const CustomNetworkImage(
-          url:
-              'https://www.foodiesfeed.com/wp-content/uploads/2024/01/best-burger-in-town.jpg'),
+      child: SizedBox(
+        width: size.width,
+        height: 320,
+        child: const ClipRRect(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(45),
+              bottomRight: Radius.circular(45)),
+          child: CustomNetworkImage(
+              fit: BoxFit.cover,
+              url:
+                  'https://www.foodiesfeed.com/wp-content/uploads/2024/01/best-burger-in-town.jpg'),
+        ),
+      ),
     ));
   }
 }
--- food_deliver_app/lib/component/custom_image.dart
+++ food_deliver_app/lib/component/custom_image.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
 class CustomNetworkImage extends StatelessWidget {
-  const CustomNetworkImage({
-    super.key,
-    required this.url,
-  });
+  const CustomNetworkImage(
+      {super.key, required this.url, this.width, this.heigth, this.fit});
 
   final String url;
+  final double? width;
+  final double? heigth;
+  final BoxFit? fit;
   @override
   Widget build(BuildContext context) {
     return Image.network(
       url,
+      width: width,
+      height: heigth,
+      fit: fit,
       loadingBuilder: (context, child, loadingProgress) {
         if (loadingProgress == null) return child;
 
